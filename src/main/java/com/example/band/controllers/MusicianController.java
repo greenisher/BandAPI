61e20ee9c79a0f8f3552b8d5833436bfab330ef0
bgreen2 <bgreen@and.digital>
2019-03-15 11:21:09 +0000

    Deleted unused code, optimised imports.

--- src/main/java/com/example/band/controllers/MusicianController.java
+++ src/main/java/com/example/band/controllers/MusicianController.java
@@ -1,40 +1,20 @@
 package com.example.band.controllers;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.example.band.exceptions.MusicianConflictError;
 import com.example.band.exceptions.MusicianNotFoundException;
-import com.example.band.repositories.MusicianRepository;
-import com.example.band.resources.MusicianResourceAssembler;
 import com.example.band.models.Musician;
 import com.example.band.services.MusicianService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import java.util.List;
 
 @RestController
 public class MusicianController {
-
-    private final MusicianRepository repository;
-
-    private final MusicianResourceAssembler assembler;
-
-    MusicianController(MusicianRepository repository, MusicianResourceAssembler assembler) {
-        this.repository = repository;
-        this.assembler = assembler;
-    }
 
     @Autowired
     MusicianService musicianService;
@@ -46,13 +26,6 @@ public class MusicianController {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<List<Musician>>(musicians, HttpStatus.OK);
-/*    public Resources<Resource<Musician>> all() {
-        List<Resource<Musician>> musicians = repository.findAll().stream()
-                .map(assembler::toResource)
-                .collect(Collectors.toList());
-
-        return new Resources<>(musicians,
-                linkTo(methodOn(MusicianController.class).all()).withSelfRel());*/
     }
 
     @GetMapping("/musicians/{id}")
@@ -62,12 +35,6 @@ public class MusicianController {
             return new ResponseEntity(new MusicianNotFoundException(id), HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<Musician>(musician, HttpStatus.OK);
-/*    public Resource<Musician> one(@PathVariable Long id) throws MusicianNotFoundException {
-
-        Musician musician = repository.findById(id)
-                .orElseThrow(() -> new MusicianNotFoundException(id));
-
-        return assembler.toResource(musician);*/
     }
 
     @PostMapping("/musicians")
@@ -80,13 +47,6 @@ public class MusicianController {
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(ucBuilder.path("/musicians/{id}").buildAndExpand(musician.getId()).toUri());
         return new ResponseEntity<String>(headers, HttpStatus.CREATED);
-/*    public ResponseEntity<?> newMusician(@RequestBody Musician newMusician) throws URISyntaxException {
-
-        Resource<Musician> resource = assembler.toResource(repository.save(newMusician));
-
-        return ResponseEntity
-                .created(new URI(resource.getId().expand().getHref()))
-                .body(resource);*/
     }
 
 
@@ -104,25 +64,6 @@ public class MusicianController {
 
         musicianService.updateMusician(updatedMusician);
         return new ResponseEntity<Musician>(updatedMusician, HttpStatus.OK);
-    /*ResponseEntity<?> replaceMusician(@RequestBody Musician newMusician, @PathVariable Long id) throws URISyntaxException {
-
-        Musician updatedMusician = repository.findById(id)
-                .map(musician -> {
-                    musician.setBandName(newMusician.getBandName());
-                    musician.setAlbums(newMusician.getAlbums());
-                    musician.setActive(newMusician.getActive());
-                    return repository.save(musician);
-                })
-                .orElseGet(() -> {
-                    newMusician.setId(id);
-                    return repository.save(newMusician);
-                });
-
-        Resource<Musician> resource = assembler.toResource(updatedMusician);
-
-        return ResponseEntity
-                .created(new URI(resource.getId().expand().getHref()))
-                .body(resource);*/
     }
 
     @DeleteMapping("/musicians/{id}")
